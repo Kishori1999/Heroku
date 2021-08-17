b2585ccc14db2009a0f307dd4d23963610c6515c
kishori <kishoriagrawal20@gmail.com>
2021-08-17 17:45:51 +0530

    checking

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express")
-const app= express()
+const app = express()
 
 app.get("/", function(req,res){
     res.send("WORKING!!!")
 })
 
-app.listen(process.eny.PORT || 5000);
+app.listen(process.env.PORT || 5000);
